08c3ab34a138bb72b1e7e5a3c26d852a95519d90
Lim Zi Yang <limziyang8@gmail.com>
2021-01-09 00:22:26 +0800

    Add axios call to orders

--- backend/data/orders.js
+++ backend/data/orders.js
@@ -1,20 +1,18 @@
-var ordersInfo = { 
-    id: 2,
-    orders: { 
-        1: [
-            {
-                orderId: 1,
-                name: "bob", 
-                postingId: 1, 
-                address: "NUS", 
-                amount: 2, 
-                status: "pending", 
-                date: "12/12/20"
-            }
-        ]
-    }
+var ordersInfo = {
+  id: 2,
+  orders: {
+    1: {
+      orderId: 1,
+      name: "bob",
+      postingId: 1,
+      address: "NUS",
+      amount: 2,
+      status: "pending",
+      date: "12/12/20",
+    },
+  },
 };
 
-module.exports = function() {
-    return ordersInfo;
-}
+module.exports = function () {
+  return ordersInfo;
+};
