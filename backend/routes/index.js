974deec8952987cdf9a91a94d18ebaa528620255
siangernlow <65005806+siangernlow@users.noreply.github.com>
2021-01-08 11:46:05 +0800

    Create order.js and posting.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const app = express();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('hi coffee here');
 });
 
 module.exports = router;
